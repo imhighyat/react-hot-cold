dac1f3f829b4266a9d6853946fe11de1c46b7f10
Hayat Mazzarella <imhighyat@gmail.com>
2018-02-21 02:11:08 -0800

    added solution to the drill

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const RESTART_GAME = 'RESTART_GAME';
+export const restartGame = correctAnswer => ({
+	type: RESTART_GAME,
+	correctAnswer
+});
+
+export const MAKE_GUESS = 'MAKE_GUESS'; 
+export const makeGuess = guess => ({
+	type: MAKE_GUESS,
+	guess
+});
+
+export const AURAL_UPDATE = 'AURAL_UPDATE'; 
+export const auralUpdate = () => ({
+	type: AURAL_UPDATE
+});
